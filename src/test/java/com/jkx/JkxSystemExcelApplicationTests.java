dabb42c382cedb847d3af66c1e9927a8b8e74a89
Mr-Lei-Xuesong <228389787@qq.com>
2020-09-17 17:40:15 +0800

    添加测试类

---
+++ src/test/java/com/jkx/JkxSystemExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkxSystemExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
